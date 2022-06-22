6e514d06294ae73022fdf47335c1ce6ff8bc4e71
ARUNKUMAR T R <arunkumartr95@gmail.com>
2022-06-22 15:11:44 +0530

    code updated

--- Dtos/ChatbotMessageReceiver.cs
+++ Dtos/ChatbotMessageReceiver.cs
@@ -37,18 +37,41 @@
         /// Gets or sets the DateTime.
         /// </summary>
         public DateTime DateTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the OptionData.
+        /// </summary>
         public List<Options> OptionData { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether IsAnswer.
+        /// </summary>
         public bool IsAnswer { get; set; }
 
         /// <summary>
         /// Gets or sets the IsAuto.
         /// </summary>
         public bool? IsAuto { get; set; }
+
+        /// <summary>
+        /// Gets or sets the QuestionId.
+        /// </summary>
         public long? QuestionId { get; set; }
     }
+
+    /// <summary>
+    /// Defines the <see cref="Options" />.
+    /// </summary>
     public class Options
     {
+        /// <summary>
+        /// Gets or sets the Id.
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ItemName.
+        /// </summary>
         public int ItemName { get; set; }
     }
 }
