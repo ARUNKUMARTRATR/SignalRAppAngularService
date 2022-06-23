546b2708d48c338e40a36b76e964787633080117
SanidhVSExp <103560762+SanidhVSExp@users.noreply.github.com>
2022-06-23 10:29:16 +0530

    Changes made to integrate API call to ClaimTrue for querying claim using InsuredName and contact number and code changes for rending options

--- Hubs/MessageHub.cs
+++ Hubs/MessageHub.cs
@@ -7,6 +7,7 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Net.Http;
     using System.Text.Json;
     using System.Threading.Tasks;
 
@@ -60,8 +61,15 @@
             _dbContext.ChatBotConversationHeaderInfo.Add(convHeader);
             _dbContext.SaveChanges();
             var convHeaderId = _dbContext.ChatBotConversationHeaderInfo.FirstOrDefault(x => x.ConversationId == channelIdentiifier).ConversationHeaderId;
-            /*var questionDetails = _dbContext.ChatBotCollectQuestionInfo.FirstOrDefault(x => x.QuestionId == message.QuestionId)*/;
-            var questionDetails = _dbContext.ChatBotCollectQuestionInfo.FirstOrDefault(x => x.ParentQuestionId == message.QuestionId);
+            var questionDetails = new ChatBotCollectQuestion();
+            if (message.QuestionId == null || message.QuestionId == 0)
+            {
+                questionDetails = _dbContext.ChatBotCollectQuestionInfo.FirstOrDefault(x => x.QuestionId == 1);
+            }
+            else
+            {
+                questionDetails = _dbContext.ChatBotCollectQuestionInfo.FirstOrDefault(x => x.QuestionId == message.QuestionId);
+            }
             question = questionDetails.QuestionName;
             
 
@@ -69,11 +77,12 @@
             {
                 ConversationHeaderId = convHeaderId,
                 QuestionName = question,
-                Answer = message.Message
+                Answer = message.UserData
             };
             _dbContext.ChatBotConversationDetailInfo.Add(conDetails);
             _dbContext.SaveChanges();
 
+            var optionGroupId = 0;
             List<ChatBotCollectQuestion> chatbotQuestion = new List<ChatBotCollectQuestion>();
             chatbotQuestion = _dbContext.ChatBotCollectQuestionInfo.Where(x => x.BotId == 1).ToList();
             ChatbotMessageReceiverDto returnMessage = new ChatbotMessageReceiverDto();
@@ -82,10 +91,29 @@
                 returnMessage.AnswerType = chatbotQuestion.FirstOrDefault(x => x.ParentQuestionId == 0).QuestionType;
                 returnMessage.Message = chatbotQuestion.FirstOrDefault(x => x.ParentQuestionId == 0).Question;
                 returnMessage.QuestionId = chatbotQuestion.FirstOrDefault(x => x.ParentQuestionId == 0).QuestionId;
+                optionGroupId = (int)chatbotQuestion.FirstOrDefault(x => x.ParentQuestionId == 0).OptionGroupId;
             }
-            //else if (message.QuestionId == 5)
-            //{
-            //}
+            else if (question == "yes_policy_number")
+            {
+                var insuredName = _dbContext.ChatBotConversationDetailInfo.FirstOrDefault(x => x.ConversationHeaderId == convHeaderId && x.QuestionName == "yes_policy_number").Answer;
+                var contactNumber = _dbContext.ChatBotConversationDetailInfo.FirstOrDefault(x => x.ConversationHeaderId == convHeaderId && x.QuestionName == "phone_number").Answer;
+                var decisionIdentifier = getClaimExistsStatus(insuredName, contactNumber);
+                var childQuestions = _dbContext.ChatBotCollectQuestionInfo.Where(x => x.ParentQuestionId == message.QuestionId).ToList();
+                var childQuestionId = 0;
+                if (childQuestions.Count > 1)
+                {
+                    childQuestionId = (int)_dbContext.ChatBotCollectQuestionInfo.FirstOrDefault(x => x.BotId == 1 && x.ParentQuestionId == message.QuestionId && x.DecisionIdentifier == decisionIdentifier).QuestionId;
+                }
+                else
+                {
+                    childQuestionId = (int)_dbContext.ChatBotCollectQuestionInfo.FirstOrDefault(x => x.BotId == 1 && x.ParentQuestionId == message.QuestionId).QuestionId;
+                }
+                optionGroupId = (int)chatbotQuestion.FirstOrDefault(x => x.QuestionId == childQuestionId).OptionGroupId;
+                returnMessage.AnswerType = chatbotQuestion.FirstOrDefault(x => x.QuestionId == childQuestionId).QuestionType;
+                //returnMessage.AnswerType = "options";
+                returnMessage.Message = chatbotQuestion.FirstOrDefault(x => x.QuestionId == childQuestionId).Question;
+                returnMessage.QuestionId = chatbotQuestion.FirstOrDefault(x => x.QuestionId == childQuestionId).QuestionId;
+            }
             else
             {
                 var childQuestions = _dbContext.ChatBotCollectQuestionInfo.Where(x => x.ParentQuestionId == message.QuestionId).ToList();
@@ -98,17 +126,23 @@
                 {
                     childQuestionId = (int)_dbContext.ChatBotCollectQuestionInfo.FirstOrDefault(x => x.BotId == 1 && x.ParentQuestionId == message.QuestionId).QuestionId;
                 }
+                optionGroupId = (int)chatbotQuestion.FirstOrDefault(x => x.QuestionId == childQuestionId).OptionGroupId;
                 returnMessage.AnswerType = chatbotQuestion.FirstOrDefault(x => x.QuestionId == childQuestionId).QuestionType;
                 //returnMessage.AnswerType = "options";
                 returnMessage.Message = chatbotQuestion.FirstOrDefault(x => x.QuestionId == childQuestionId).Question;
                 returnMessage.QuestionId = chatbotQuestion.FirstOrDefault(x => x.QuestionId == childQuestionId).QuestionId;
             }
+            var optionList = _dbContext.ChatBotQuestionOptionGroupCodeInfo.Where(x => x.OptionGroupId == optionGroupId).Select(x => new Options
+            {
+                Id = (int)x.OptionCodeId,
+                ItemName = x.Code
+            }).ToList();
+            returnMessage.OptionData = optionList;
             returnMessage.MessageUserType = 0;
             returnMessage.ConnectionId = message.ConnectionId;
             returnMessage.IsAnswer = false;
             returnMessage.IsAuto = false;
             returnMessage.DateTime = DateTime.Now;
-
             await Clients.Caller.SendAsync("RecieveMsg", returnMessage);
         }
 
@@ -131,6 +165,30 @@
         {
             await Clients.All.SendAsync("onUserDisconnected", Context.ConnectionId);
             await base.OnDisconnectedAsync(exception);
+        }
+
+        private string getClaimExistsStatus(string insuredName, string phoneNumber)
+        {
+            HttpClient httpClient = new HttpClient();
+            var urlString = "http://localhost:49414/ChatBot/checkPhoneAndInsuredName?insuredName=" + insuredName + "&phoneNumber=" + phoneNumber;
+            Uri u = new Uri(urlString);
+            HttpRequestMessage request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = u,
+            };
+            //var req = httpClient.SendAsync(request);
+            var task = Task.Run(() => httpClient.SendAsync(request));
+            task.Wait();
+            var response = task.Result.StatusCode;
+            if(response.ToString() == "OK")
+            {
+                return "true";
+            }
+            else
+            {
+                return "false";
+            }
         }
     }
 }
